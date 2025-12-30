480c3b00ea9337e13c3d7cc68ea8e57af8d7ad3d
lvxing-svg <mail_lvxing@163.com>
2025-12-30 21:50:02 +0800

    Update build.sh

--- immortalwrt/build.sh
+++ immortalwrt/build.sh
@@ -5,6 +5,17 @@ PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES kmod-tun"
+PACKAGES="$PACKAGES kmod-inet-diag"
+PACKAGES="$PACKAGES kmod-nft-tproxy"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn
+PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+PACKAGES="$PACKAGES luci-app-uhttpd
 
-
-make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="512"
+make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="4096"
